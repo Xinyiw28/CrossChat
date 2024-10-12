0f0d0f50555ee66a847df8996a0339bc007f6cf7
albert <tyc7654321@gmail.com>
2024-10-12 00:15:51 -0700

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,8 +7,6 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'CrossChat'
 copyright = '2024, Xinyi Wang'
